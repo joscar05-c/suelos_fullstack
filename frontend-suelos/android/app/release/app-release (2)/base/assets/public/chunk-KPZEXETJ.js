8e4bbd303791128ca256942f7dda9c4b510cbfdf
joscar05-c <pizangojosec@gmail.com>
2026-04-12 12:29:43 -0500

    android

---
+++ frontend-suelos/android/app/release/app-release (2)/base/assets/public/chunk-KPZEXETJ.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-2PVGTIXG.js";import"./chunk-D7OODA4R.js";import"./chunk-ET3KJGA5.js";import"./chunk-HWOOGQPP.js";import"./chunk-CKP3SGE2.js";import"./chunk-AMHURBE3.js";import"./chunk-YDOXD4O3.js";export{b as iosTransitionAnimation,a as shadow};
